0480b007648d7660ef382c8c2325024708aedf5a
Ronnie1320 <smartpower0105@gmail.com>
2023-03-10 15:34:11 +0100

    fix encoding

--- scripts/parse_csv.py
+++ scripts/parse_csv.py
@@ -12,6 +12,6 @@ with open(input_, "r", encoding="cp1252") as f:
     reader = csv.reader(f, delimiter="\t")
     csv_list = list(reader)
 
-with open(output_, "w", encoding="utf-8") as f:
+with open(output_, "w") as f:
     data = json.dumps(csv_list)
     f.write(data.replace("/", "*"))
