836b24f376a8b9139de0a414dca9206b33108fa8
Rene Regensbogen <68396806+Durus-R@users.noreply.github.com>
2023-04-16 16:27:27 +0200

    init devenv

--- devenv.nix
+++ devenv.nix
@@ -1,28 +1,26 @@
 { pkgs, ... }:
 
 {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
-
+  
   # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  packages = [  pkgs.git 
+                pkgs.nodejs-18_x 
+                pkgs.nodePackages.node-gyp-build 
+                pkgs.gcc9
+                pkgs.python310Full 
+                pkgs.gnumake
+                pkgs.python310Packages.gyp];
 
   # https://devenv.sh/scripts/
-  scripts.hello.exec = "echo hello from $GREET";
+  scripts.update.exec = "npm ci";
 
   enterShell = ''
-    hello
-    git --version
+    echo Use update to update to the latest packages.
+    echo Node Version: `node --version`
   '';
 
-  # https://devenv.sh/languages/
-  # languages.nix.enable = true;
-
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.ping.exec = "ping example.com";
-
+  processes = {
+    node.exec= "npm run watch:start";
+  };
   # See full reference at https://devenv.sh/reference/options/
 }
