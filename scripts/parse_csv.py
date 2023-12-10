343d3145a7dbec70a86ac210b47c7e7150b88987
Durus-R <smartpower0105@gmail.com>
2023-12-10 15:02:43 +0100

    update losungen for next year

--- scripts/parse_csv.py
+++ scripts/parse_csv.py
@@ -1,17 +1,23 @@
 import csv, sys, json, os
 from datetime import datetime
 
-if len(sys.argv)==1:
-    input_ = "losungen_{}.csv".format(datetime.now().year)
-    output_ = os.path.join(os.getcwd(), "dist/losungen_{}.json".format(datetime.now().year))
-else:
-    input_ = sys.argv[1]
-    output_ = sys.argv[2]
+def write_file_for_year(year : int=datetime.now().year):
+    input_ = "losungen_{}.csv".format(year)
+    output_ = os.path.join(os.getcwd(), "dist/losungen_{}.json".format(year))
+    rewrite_file(input_, output_)
 
-with open(input_, "r", encoding="cp1252") as f:
-    reader = csv.reader(f, delimiter="\t")
-    csv_list = list(reader)
+def rewrite_file(i : str, o : str):
+    with open(i, "r", encoding="cp1252") as f:
+        reader = csv.reader(f, delimiter="\t")
+        csv_list = list(reader)
 
-with open(output_, "w", encoding="utf-8") as f:
-    data = json.dumps(csv_list)
-    f.write(data.replace("/", "*"))
+    with open(o, "w", encoding="utf-8") as f:
+        data = json.dumps(csv_list)
+        f.write(data.replace("/", "*"))
+
+write_file_for_year()
+
+next_year = datetime.now().year+1
+
+if os.path.exists("losungen_{}.csv".format(next_year)):
+    write_file_for_year(next_year)
